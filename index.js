3db4a24b30b4b9d4c68376f57e53ee1280af6252
zuleka abdirashid <zulexzulex90@gmail.com>
2024-12-08 10:38:50 +0300

    adds

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet (someValue)  {
+   if (someValue <= 400) {
+    return "This one is on me!" ;
+   } else  if (someValue > 400 && someValue <= 2000 ) {
+    return "That will be twenty bucks." ;
+   } else if (someValue > 2000 && someValue <= 2500 ) {
+    return "I will gladly take your thirty bucks." ;
+   } else {
+    return "No can do." ;
+   }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(City) {
+  return City ==="NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much." ;
+      case "not as generous":
+        return "Thank you.";
+        default:
+          return "Bye." ;
+  }
 }
